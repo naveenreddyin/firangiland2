08c7af81dcaa46a821a4d754503895baff74f103
naveen <naveenreddyin@gmail.com>
2016-03-07 23:15:05 +0100

    added more modules and dump4.sql

---
+++ themes/bartik/templates/views-exposed-form-frontpage-page.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 07.03.16
+ * Time: 21:44
+ */
+?>
+<?php if ($exposed): ?>
+    <div class="view-filters">
+        <?php print $exposed; ?>
+    </div>
+<?php endif; ?>
+
