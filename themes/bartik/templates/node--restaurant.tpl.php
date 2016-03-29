783d8edd196bc30fff7f98ced41b94b1bc414ba0
naveenreddyin <naveenreddyin@gmail.com>
2016-03-29 14:12:55 +0200

    user can only vote once

--- themes/bartik/templates/node--restaurant.tpl.php
+++ themes/bartik/templates/node--restaurant.tpl.php
@@ -1,7 +1,7 @@
 <?php
 dpm($content);
-print render($content['field_tet']);
-print render($content['rate_thumbs_up_down']);
+// print render($content['field_tet']);
+// print render($content['rate_thumbs_up_down']);
 if($title){
 
 	print '<h1 class="title" id="page-title">';
@@ -10,7 +10,19 @@ if($title){
 
 }
 
+if(firangiland_custom_check_if_user_has_reviewed()){
+	$query = firangiland_custom_check_if_user_has_reviewed();
+	$nid = $query['firangiland_custom_user_votes_status_review_nid'];
+	print 'You already have a review for this restaurant.';
+
+	print "<a href='".base_path()."node/".$nid."'>Want to modify?</a>";
+
+}else{
 print render($content['links']);
+}
+
+// print render($content['links']);
+// print "<a href=".base_path()."node/add/review-for-restaurant>click</a>";
 
 $criteria_for_count = array(
 		'entity_id' => $node->nid,
